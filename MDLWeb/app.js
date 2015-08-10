c3db39165ae2cc860bb8b9b8285a24506839d7a4
Tiago de Mello Britto <obritto@hotmail.com>
2015-08-10 20:53:15 -0300

    MDL Google

---
+++ MDLWeb/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('app', []);
+
+app.controller('TesteCtrl', function TesteCtrl($scope) {
+
+    $scope.numMsg = 6;
+
+    $scope.mais = function () {
+        $scope.numMsg = $scope.numMsg + 1;
+    };
+
+});
